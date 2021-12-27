556e2f623339d7374d880f16972892e3c00d0934
paolomococci <paolomococci@gmail.com>
2021-12-27 06:48:37 +0100

    NativeComplexNumberResourceIT class belonging to project complex-number-resource has been added

---
+++ complex-number-resource/src/native-test/java/local/example/rest/json/NativeComplexNumberResourceIT.java
@@ -0,0 +1,8 @@
+package local.example.rest.json;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeComplexNumberResourceIT
+        extends ComplexNumberResourceTest {
+}
