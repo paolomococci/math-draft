1e4a303f93ec40754ec269e59c4efb7a41dc100e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2022-02-01 06:41:11 +0100

    Delete NativeComplexNumberResourceIT.java

--- complex-number-resource/src/native-test/java/local/example/rest/json/NativeComplexNumberResourceIT.java
+++
@@ -1,8 +0,0 @@
-package local.example.rest.json;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeComplexNumberResourceIT
-        extends ComplexNumberResourceTest {
-}
