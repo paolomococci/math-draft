29d7e9af4d15f86a72436babeffc5809a59ca0c7
paolomococci <paolomococci@gmail.com>
2022-02-04 10:03:58 +0100

    edited class Gcd belonging to project gcd

--- gcd/src/main/java/local/example/outcome/Gcd.java
+++ gcd/src/main/java/local/example/outcome/Gcd.java
@@ -1,4 +1,23 @@
 package local.example.outcome;
 
+import javax.inject.Singleton;
+
+@Singleton
 public class Gcd {
+
+    public int a;
+    public int b;
+    public int gcd;
+
+    public void setGcd() {
+        this.gcd = this.greatestCommonDivisor(this.a, this.b);
+    }
+
+    private int greatestCommonDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        int temp = a % b;
+        return greatestCommonDivisor(b, temp);
+    }
 }
