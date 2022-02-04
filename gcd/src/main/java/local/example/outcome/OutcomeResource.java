d41b58aab43535ed9c3c7cfeeb6a7efdc947a924
paolomococci <paolomococci@gmail.com>
2022-02-04 10:03:48 +0100

    edited class OutcomeResource belonging to project gcd

--- gcd/src/main/java/local/example/outcome/OutcomeResource.java
+++ gcd/src/main/java/local/example/outcome/OutcomeResource.java
@@ -2,11 +2,15 @@ package local.example.outcome;
 
 import io.quarkus.vertx.web.Route;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
 @Path(value = "/outcome")
 public class OutcomeResource {
+
+    @Inject
+    Gcd gcd;
 
     @Route(
             methods = Route.HttpMethod.GET,
@@ -18,7 +22,9 @@ public class OutcomeResource {
 
     @Route(
             path = "/gcd",
-            methods = Route.HttpMethod.POST
+            methods = Route.HttpMethod.POST,
+            consumes = MediaType.APPLICATION_JSON,
+            produces = MediaType.APPLICATION_JSON
     )
     public Gcd gcd() {
         return new Gcd();
