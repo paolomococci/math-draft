3e208c9b97f3940bed1adc7fbd7c2563f7d2188d
paolomococci <paolomococci@gmail.com>
2022-02-05 11:54:42 +0100

    edited class Lcm belonging to project lcm

--- lcm/src/main/java/local/example/outcome/model/Lcm.java
+++ lcm/src/main/java/local/example/outcome/model/Lcm.java
@@ -13,4 +13,22 @@ public class Lcm {
         this.a = Math.abs(a);
         this.b = Math.abs(b);
     }
+
+    public void setLcm() {
+        this.lcm = this.leastCommonMultiple(this.a, this.b);
+    }
+
+    private int leastCommonMultiple(int a, int b) {
+        if (a == 0 || b == 0) {
+            return a + b;
+        }
+
+        int higher = Math.max(a, b);
+        int lower = Math.min(a, b);
+        int lcm = higher;
+        while (lcm % lower != 0) {
+            lcm += higher;
+        }
+        return lcm;
+    }
 }
