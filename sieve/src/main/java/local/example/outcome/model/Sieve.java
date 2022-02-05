3ae78a36c74666348ef71bc8e0413a7bb7841c14
paolomococci <paolomococci@gmail.com>
2022-02-05 23:00:15 +0100

    edited class Sieve belonging to project sieve

--- sieve/src/main/java/local/example/outcome/model/Sieve.java
+++ sieve/src/main/java/local/example/outcome/model/Sieve.java
@@ -1,17 +1,45 @@
 package local.example.outcome.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Sieve {
 
-    public long a;
-    public List<Boolean> prime;
+    public int top;
+    public List<Integer> primes = new ArrayList<>();
 
     public Sieve() {
     }
 
-    public Sieve(long a, List<Boolean> prime) {
-        this.a = a;
-        this.prime = prime;
+    public Sieve(int top) {
+        this.top = top;
+    }
+
+    public void setPrimes() {
+        this.primes = this.sift();
+    }
+
+    private List<Integer> sift() {
+        int max = this.top;
+        List<Integer> primes = new ArrayList<>();
+        boolean[] integers = new boolean[max+1];
+
+        Arrays.fill(integers, true);
+
+        for (int external = 2; external*external <= max; external++) {
+            if (integers[external]) {
+                for (int internal = external*external; internal <= max; internal += external) {
+                    integers[internal] = false;
+                }
+            }
+        }
+
+        for (int index = 2; index <= max; index++) {
+            if (integers[index]) {
+                primes.add(index);
+            }
+        }
+        return primes;
     }
 }
