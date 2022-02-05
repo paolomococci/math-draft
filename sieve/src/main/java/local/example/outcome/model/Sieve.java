e66ed411f38b1e78ffe357a5004aaf123bf4b6fa
paolomococci <paolomococci@gmail.com>
2022-02-05 19:55:01 +0100

    edited class Sieve belonging to project sieve

--- sieve/src/main/java/local/example/outcome/model/Sieve.java
+++ sieve/src/main/java/local/example/outcome/model/Sieve.java
@@ -1,4 +1,17 @@
 package local.example.outcome.model;
 
+import java.util.List;
+
 public class Sieve {
+
+    public long a;
+    public List<Boolean> prime;
+
+    public Sieve() {
+    }
+
+    public Sieve(long a, List<Boolean> prime) {
+        this.a = a;
+        this.prime = prime;
+    }
 }
