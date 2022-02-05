48fdfc5d610bf14b03e15219d7fb550698ac506e
paolomococci <paolomococci@gmail.com>
2022-02-05 11:16:38 +0100

    edited class Lcm belonging to project lcm

--- lcm/src/main/java/local/example/outcome/model/Lcm.java
+++ lcm/src/main/java/local/example/outcome/model/Lcm.java
@@ -1,4 +1,16 @@
 package local.example.outcome.model;
 
 public class Lcm {
+
+    public int a;
+    public int b;
+    public int lcm;
+
+    public Lcm() {
+    }
+
+    public Lcm(int a, int b) {
+        this.a = Math.abs(a);
+        this.b = Math.abs(b);
+    }
 }
