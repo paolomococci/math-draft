cd6067e0cb90e6022a45e1aa0e8af76f719187da
paolomococci <paolomococci@gmail.com>
2022-02-05 20:43:38 +0100

    edited class OutcomeResource belonging to project sieve

--- sieve/src/main/java/local/example/outcome/OutcomeResource.java
+++ sieve/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,19 +1,38 @@
 package local.example.outcome;
 
-import io.smallrye.mutiny.Uni;
 import local.example.outcome.model.Sieve;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/outcome")
 public class OutcomeResource {
 
+    private final Set<Sieve> sieves = Collections.newSetFromMap(
+            Collections.synchronizedMap(
+                    new LinkedHashMap<>()
+            )
+    );
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Sieve> read() {
-        return null;
+    public Response read() {
+        if (sieves.isEmpty()) {
+            return Response.ok().build();
+        }
+        return Response.ok(sieves).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<Sieve> compute(Sieve sieve) {
+        sieve.setPrime();
+        sieves.add(sieve);
+        return sieves;
     }
 }
