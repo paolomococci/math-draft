8b776251b3f95af3cae94b4075cfcb410fc00161
paolomococci <paolomococci@gmail.com>
2022-02-09 17:36:31 +0100

    started editing class Quicksort belonging to project quicksort

--- quicksort/src/main/java/local/example/outcome/algorithm/Quicksort.java
+++ quicksort/src/main/java/local/example/outcome/algorithm/Quicksort.java
@@ -1,4 +1,12 @@
 package local.example.outcome.algorithm;
 
 public class Quicksort {
+
+    public static void quickSort() {
+
+    }
+
+    private static long partition() {
+        return 0L;
+    }
 }
