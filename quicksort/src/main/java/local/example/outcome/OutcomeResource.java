debc1014c3a2b11759b1d2c7956f713ca7ab8586
paolomococci <paolomococci@gmail.com>
2022-02-09 16:52:25 +0100

    edited class OutcomeResource belonging to project quicksort

--- quicksort/src/main/java/local/example/outcome/OutcomeResource.java
+++ quicksort/src/main/java/local/example/outcome/OutcomeResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/outcome")
 public class OutcomeResource {
@@ -12,5 +13,12 @@ public class OutcomeResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String feedback() {
         return "-- outcome feedback --";
+    }
+
+    @GET
+    @Path("/sorted")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response read() {
+        return Response.ok().build();
     }
 }
