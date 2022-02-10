3b6d7b74fca279b7e022e1534a2a8245ab558cca
paolomococci <paolomococci@gmail.com>
2022-02-10 16:39:20 +0100

    edited class Quicksort belonging to project quicksort

--- quicksort/src/main/java/local/example/outcome/algorithm/Quicksort.java
+++ quicksort/src/main/java/local/example/outcome/algorithm/Quicksort.java
@@ -2,11 +2,24 @@ package local.example.outcome.algorithm;
 
 public class Quicksort {
 
-    public static void quickSort(long keys[], long begin, long ending) {
+    public static void quickSort(long keys[], int begin, int ending) {
 
     }
 
-    private static long partition(long keys[], long begin, long ending) {
+    private static long partition(long keys[], int begin, int ending) {
+
+        long pivot = keys[ending];
+        int index = begin-1;
+
+        for (int i = 0; i < ending; i++) {
+            if (keys[i] <= pivot) {
+                index++;
+                long swap = keys[index];
+                keys[index] = keys[i];
+                keys[i] = swap;
+            }
+        }
+
         return 0L;
     }
 }
