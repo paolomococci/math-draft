7362d227b702461a85aeef795266500ab76079e7
paolomococci <paolomococci@gmail.com>
2022-02-13 20:22:49 +0100

    edited class QuicksortTests belonging to project quicksort

--- quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
+++ quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
@@ -1,6 +1,8 @@
 package local.example.outcome.algorithm;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -8,6 +10,10 @@ public class QuicksortTests {
 
     @Test
     public void rawTest() {
-
+        // TODO
+        long[] messy = {};
+        long[] tidy = {};
+        Quicksort.quickSort(messy, 0, messy.length-1);
+        Assert.assertArrayEquals(tidy, messy);
     }
 }
