f998c7d98fea7fd7a4dd16f849320be9b1446c56
paolomococci <paolomococci@gmail.com>
2022-02-14 18:04:14 +0100

    edited class OutcomeResource belonging to project quicksort

--- quicksort/src/main/java/local/example/outcome/OutcomeResource.java
+++ quicksort/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,17 +1,19 @@
 package local.example.outcome;
 
 import local.example.outcome.algorithm.Quicksort;
-import local.example.outcome.model.Item;
+import local.example.outcome.model.Assortment;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/outcome")
 public class OutcomeResource {
 
-    private final Set<List<Item>> itemsSet = Collections.newSetFromMap(
+    private final Set<Assortment> assortments = Collections.newSetFromMap(
             Collections.synchronizedMap(
                     new LinkedHashMap<>()
             )
@@ -27,19 +29,19 @@ public class OutcomeResource {
     @Path("/items")
     @Produces(MediaType.APPLICATION_JSON)
     public Response read() {
-        if (itemsSet.isEmpty()) {
+        if (assortments.isEmpty()) {
             return Response.ok().build();
         }
-        return Response.ok(itemsSet).build();
+        return Response.ok(assortments).build();
     }
 
     @POST
     @Path("/sorting")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Set<List<Item>> sorting(List<Item> items) {
-        Quicksort.quickSort(items);
-        itemsSet.add(items);
-        return itemsSet;
+    public Set<Assortment> sorting(Assortment assortment) {
+        //Quicksort.quickSort(assortment);
+        assortments.add(assortment);
+        return assortments;
     }
 }
