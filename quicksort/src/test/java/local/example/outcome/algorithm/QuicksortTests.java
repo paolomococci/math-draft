e75baab207124966d57e14b2690f6869308e9798
paolomococci <paolomococci@gmail.com>
2022-02-14 10:31:16 +0100

    edited class QuicksortTests belonging to project quicksort

--- quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
+++ quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
@@ -11,8 +11,8 @@ public class QuicksortTests {
     @Test
     public void rawTest() {
         // TODO
-        long[] messy = {};
-        long[] tidy = {};
+        long[] messy = {9, 7, 1, 0, 8, 2, 3, 4, 5, 6, 6};
+        long[] tidy = {0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9};
         Quicksort.quickSort(messy, 0, messy.length-1);
         Assertions.assertArrayEquals(tidy, messy);
     }
