28a53fdf4f7176d4607d14cd497b349e66b7cb01
paolomococci <paolomococci@gmail.com>
2022-02-15 10:40:18 +0100

    edited class Assortment belonging to project quicksort

--- quicksort/src/main/java/local/example/outcome/model/Assortment.java
+++ quicksort/src/main/java/local/example/outcome/model/Assortment.java
@@ -1,8 +1,24 @@
 package local.example.outcome.model;
 
-import java.util.List;
+import java.util.*;
 
 public class Assortment {
+    
+    public final List<Item> items;
 
-    public List<Item> items;
+    public Assortment() {
+        items = new ArrayList<>();
+    }
+
+    public void add(Item item) {
+        this.items.add(item);
+    }
+
+    public Item get(int index) {
+        return this.items.get(index);
+    }
+
+    public List<Item> toList() {
+        return this.items.stream().toList();
+    }
 }
