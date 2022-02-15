f2c81c330067c411f74334d6484293c133e2f5aa
paolomococci <paolomococci@gmail.com>
2022-02-15 10:39:18 +0100

    edited class QuicksortTests belonging to project quicksort

--- quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
+++ quicksort/src/test/java/local/example/outcome/algorithm/QuicksortTests.java
@@ -2,6 +2,8 @@ package local.example.outcome.algorithm;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+import local.example.outcome.model.Assortment;
+import local.example.outcome.model.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +11,32 @@ import org.junit.jupiter.api.Test;
 public class QuicksortTests {
 
     @Test
-    public void rawTest() {
+    public void longTest() {
         long[] messy = {9, 7, 1, 0, 8, 2, 3, 4, 5, 6, 6};
         long[] tidy = {0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9};
         Quicksort.quickSort(messy, 0, messy.length-1);
         Assertions.assertArrayEquals(tidy, messy);
+    }
+
+    @Test
+    public void assortmentTest() {
+        Item one = new Item(1, "one");
+        Item two = new Item(2, "two");
+        Item three = new Item(3, "three");
+        Item four = new Item(4, "four");
+        Item five = new Item(5, "five");
+        Assortment messy = new Assortment();
+        messy.add(two);
+        messy.add(one);
+        messy.add(three);
+        messy.add(five);
+        messy.add(four);
+        Assortment tidy = new Assortment();
+        tidy.add(one);
+        tidy.add(two);
+        tidy.add(three);
+        tidy.add(four);
+        tidy.add(five);
+        // TODO
     }
 }
