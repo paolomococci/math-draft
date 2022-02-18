5b5aa826189a09b350536720c943054498dd5624
paolomococci <paolomococci@gmail.com>
2022-02-18 08:30:40 +0100

    edited class RadixsortTests belonging to project radixsort

--- radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
+++ radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
@@ -1,4 +1,13 @@
 package local.example.outcome.algorithm;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class RadixsortTests {
+
+    @Test
+    public void someTest() {
+
+    }
 }
