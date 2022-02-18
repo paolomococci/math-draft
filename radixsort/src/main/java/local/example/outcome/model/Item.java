5636b5a07e6ff58e5b25203b253c5ef20ff69d12
paolomococci <paolomococci@gmail.com>
2022-02-18 08:47:07 +0100

    edited class Item belonging to project radixsort

--- radixsort/src/main/java/local/example/outcome/model/Item.java
+++ radixsort/src/main/java/local/example/outcome/model/Item.java
@@ -1,4 +1,16 @@
 package local.example.outcome.model;
 
 public class Item {
+
+    public Integer key;
+    public String name;
+
+    public Item() {
+
+    }
+
+    public Item(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
 }
