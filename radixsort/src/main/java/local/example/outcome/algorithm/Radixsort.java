024e61dd6ff5a37e3eab697a8d4c252e8430b723
paolomococci <paolomococci@gmail.com>
2022-02-18 09:32:10 +0100

    edited class Radixsort belonging to project radixsort

--- radixsort/src/main/java/local/example/outcome/algorithm/Radixsort.java
+++ radixsort/src/main/java/local/example/outcome/algorithm/Radixsort.java
@@ -3,16 +3,28 @@ package local.example.outcome.algorithm;
 public class Radixsort {
 
     public static void radixsort(int[] messy) {
+
         int maxInt = findMaxIntFromArray(messy);
         int numberOfDigits = computeNumberOfDigits(maxInt);
         int placeholder = 1;
+
+        while (numberOfDigits-- > 0) {
+            sortOn(messy, placeholder);
+            placeholder *= 10;
+        }
+    }
+
+    private static void sortOn(int[] messy, int placeholder) {
+        // TODO
     }
 
     private static int computeNumberOfDigits(int maxInt) {
+        // TODO
         return 0;
     }
 
     private static int findMaxIntFromArray(int[] messy) {
+        // TODO
         return 0;
     }
 }
