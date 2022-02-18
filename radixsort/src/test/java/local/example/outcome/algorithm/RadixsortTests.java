b07a22534cf7b87683b2fd9da172c46dc931d656
paolomococci <paolomococci@gmail.com>
2022-02-18 08:36:25 +0100

    edited class RadixsortTests belonging to project radixsort

--- radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
+++ radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
@@ -1,13 +1,18 @@
 package local.example.outcome.algorithm;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class RadixsortTests {
 
     @Test
-    public void someTest() {
-
+    public void intTest() {
+        int[] messy = {9, 7, 1, 0, 8, 2, 3, 4, 5, 6, 6};
+        int[] tidy = {0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9};
+        // TODO
+        Assertions.assertArrayEquals(tidy, messy);
     }
 }
