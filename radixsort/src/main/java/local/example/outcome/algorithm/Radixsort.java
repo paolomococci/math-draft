efa0d9026ec39972d374ebe169be506422e903a0
paolomococci <paolomococci@gmail.com>
2022-02-19 07:13:09 +0100

    edited class Radixsort belonging to project radixsort

--- radixsort/src/main/java/local/example/outcome/algorithm/Radixsort.java
+++ radixsort/src/main/java/local/example/outcome/algorithm/Radixsort.java
@@ -4,27 +4,43 @@ public class Radixsort {
 
     public static void radixsort(int[] messy) {
 
-        int maxInt = findMaxIntFromArray(messy);
-        int numberOfDigits = computeNumberOfDigits(maxInt);
-        int placeholder = 1;
+        int maxKey = findMaxIntFromArray(messy);
 
-        while (numberOfDigits-- > 0) {
-            sortOn(messy, placeholder);
-            placeholder *= 10;
+        for (int position = 1; (maxKey/position) > 0; position *= 10) {
+            sortOn(messy, position);
         }
     }
 
-    private static void sortOn(int[] messy, int placeholder) {
-        // TODO
-    }
+    private static void sortOn(int[] messy, int position) {
+        int radix = 10;
+        int[] frequency = new int[radix];
+        int[] tidy = new int[messy.length];
 
-    private static int computeNumberOfDigits(int maxInt) {
-        // TODO
-        return 0;
+        for (int i = 0; i < messy.length; i++) {
+            int digit = (messy[i]/position)%radix;
+            frequency[digit]++;
+        }
+
+        for (int i = 1; i < radix; i++) {
+            frequency[i] += frequency[i-1];
+        }
+
+        for (int i = (messy.length-1); i >= 0; i--) {
+            int digit = (messy[i]/position)%radix;
+            tidy[frequency[digit]-1] = messy[i];
+            frequency[digit]--;
+        }
+
+        System.arraycopy(tidy, 0, messy, 0, messy.length);
     }
 
     private static int findMaxIntFromArray(int[] messy) {
-        // TODO
-        return 0;
+        int max = messy[0];
+        for (int i = 1; i < messy.length; i++) {
+            if (messy[i] > max) {
+                max = messy[i];
+            }
+        }
+        return max;
     }
 }
