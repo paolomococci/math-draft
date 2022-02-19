1bc743ed90d9ba63b8daf869c94d761de56a74c2
paolomococci <paolomococci@gmail.com>
2022-02-19 07:13:19 +0100

    edited class RadixsortTests belonging to project radixsort

--- radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
+++ radixsort/src/test/java/local/example/outcome/algorithm/RadixsortTests.java
@@ -10,8 +10,8 @@ public class RadixsortTests {
 
     @Test
     public void intTest() {
-        int[] messy = {457, 344, 320, 128, 652, 236, 543, 783, 239, 546, 215};
-        int[] tidy = {128, 215, 236, 239, 320, 344, 457, 543, 546, 652, 783};
+        int[] messy = {457, 344, 4, 320, 128, 652, 32, 236, 543, 98, 783, 239, 546, 215};
+        int[] tidy = {4, 32, 98, 128, 215, 236, 239, 320, 344, 457, 543, 546, 652, 783};
         Radixsort.radixsort(messy);
         Assertions.assertArrayEquals(tidy, messy);
     }
