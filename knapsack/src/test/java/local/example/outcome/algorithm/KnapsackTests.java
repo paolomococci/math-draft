a8a01d3c95c935675e6660d2e00a15ecb037a633
paolomococci <paolomococci@gmail.com>
2022-03-02 15:07:50 +0100

    edited class KnapsackTests belonging to project knapsack

--- knapsack/src/test/java/local/example/outcome/algorithm/KnapsackTests.java
+++ knapsack/src/test/java/local/example/outcome/algorithm/KnapsackTests.java
@@ -1,4 +1,15 @@
 package local.example.outcome.algorithm;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class KnapsackTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
