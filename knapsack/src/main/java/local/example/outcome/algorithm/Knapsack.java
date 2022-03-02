0a7ecdab700880700f6d97b425abee9ba165e6fb
paolomococci <paolomococci@gmail.com>
2022-03-02 16:44:18 +0100

    edited class Knapsack belonging to project knapsack

--- knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
+++ knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
@@ -2,17 +2,26 @@ package local.example.outcome.algorithm;
 
 public class Knapsack {
 
-    public int recursive(
+    public static int recursive(
             int[] weights,
             int[] values,
             int multiplicity,
             int weightLimit
     ) {
-        // TODO
-        return 0;
+        if (multiplicity <= 0) {
+            return 0;
+        } else if (weights[multiplicity-1] > weightLimit) {
+            return recursive(weights, values, (multiplicity-1), weightLimit);
+        } else {
+            return Math.max(
+                    recursive(weights, values, (multiplicity-1), weightLimit),
+                    recursive(weights, values, (multiplicity-1), (weightLimit-weights[multiplicity-1]))
+                            + values[multiplicity-1]
+            );
+        }
     }
 
-    public int dynamic(
+    public static int dynamic(
             int[] weights,
             int[] values,
             int multiplicity,
