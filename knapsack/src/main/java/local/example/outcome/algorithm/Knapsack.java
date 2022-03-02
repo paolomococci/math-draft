2a1b595c3a9ea1af3e26975a617a01fb60e7c51f
paolomococci <paolomococci@gmail.com>
2022-03-02 16:35:20 +0100

    edited class Knapsack belonging to project knapsack

--- knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
+++ knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
@@ -2,12 +2,22 @@ package local.example.outcome.algorithm;
 
 public class Knapsack {
 
-    public int recursive(int[] weights, int[] values, int multiplicity, int weightLimit) {
+    public int recursive(
+            int[] weights,
+            int[] values,
+            int multiplicity,
+            int weightLimit
+    ) {
         // TODO
         return 0;
     }
 
-    public int dynamic(int[] weights, int[] values, int multiplicity, int weightLimit) {
+    public int dynamic(
+            int[] weights,
+            int[] values,
+            int multiplicity,
+            int weightLimit
+    ) {
         // TODO
         return 0;
     }
