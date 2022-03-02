e696d0bc3701d5e5437b4aefa176f7e1ce0678d6
paolomococci <paolomococci@gmail.com>
2022-03-02 17:17:10 +0100

    edited class Knapsack belonging to project knapsack

--- knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
+++ knapsack/src/main/java/local/example/outcome/algorithm/Knapsack.java
@@ -27,7 +27,27 @@ public class Knapsack {
             int multiplicity,
             int weightLimit
     ) {
-        // TODO
-        return 0;
+        if (multiplicity <= 0 || weightLimit <= 0)
+            return 0;
+
+        int[][] matrix = new int[multiplicity+1][weightLimit+1];
+
+        for (int i = 0; i <= weightLimit; i++) {
+            matrix[0][i] = 0;
+        }
+
+        for (int i = 1; i <= multiplicity; i++) {
+            for (int j = 1; j <= weightLimit; j++) {
+                if (weights[i-1] > j) {
+                    matrix[i][j] = matrix[i-1][j];
+                } else {
+                    matrix[i][j] = Math.max(
+                            matrix[i-1][j],
+                            matrix[i-1][j-weights[i-1]] + values[i-1]
+                    );
+                }
+            }
+        }
+        return matrix[multiplicity][weightLimit];
     }
 }
