c91f92e299be8281dbf58addf603a4f889c3df3a
paolomococci <paolomococci@gmail.com>
2022-03-05 16:05:57 +0100

    edited class NativeKMeansIT belonging to project k-means

--- k-means/src/test/java/local/example/outcome/algorithm/NativeKMeansIT.java
+++ k-means/src/test/java/local/example/outcome/algorithm/NativeKMeansIT.java
@@ -1,4 +1,8 @@
 package local.example.outcome.algorithm;
 
-public class NativeKMeansIT {
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeKMeansIT
+        extends KMeans {
 }
