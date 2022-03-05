c6a1acaf23355f6debf256be96b1320b53d709e0
paolomococci <paolomococci@gmail.com>
2022-03-05 16:04:18 +0100

    edited class KMeansTests belonging to project k-means

--- k-means/src/test/java/local/example/outcome/algorithm/KMeansTests.java
+++ k-means/src/test/java/local/example/outcome/algorithm/KMeansTests.java
@@ -2,6 +2,14 @@ package local.example.outcome.algorithm;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 @QuarkusTest
 public class KMeansTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
