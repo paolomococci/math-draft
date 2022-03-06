41e6522b466f5612b0af64d47a749bcb7ce258c3
paolomococci <paolomococci@gmail.com>
2022-03-06 22:28:34 +0100

    edited class Edge belonging to project djp

--- djp/src/main/java/local/example/outcome/model/Edge.java
+++ djp/src/main/java/local/example/outcome/model/Edge.java
@@ -2,9 +2,14 @@ package local.example.outcome.model;
 
 public class Edge {
 
-    public int weight;
-    public boolean included;
+    public int weight = Integer.MAX_VALUE;
+    public boolean included = false;
 
     public Edge() {
+    }
+
+    public Edge(int weight, boolean included) {
+        this.weight = weight;
+        this.included = included;
     }
 }
