9b534f7cd08ec1e3f7f7453127983787cc1cf782
paolomococci <paolomococci@gmail.com>
2022-03-06 22:24:46 +0100

    edited class Vertex belonging to project djp

--- djp/src/main/java/local/example/outcome/model/Vertex.java
+++ djp/src/main/java/local/example/outcome/model/Vertex.java
@@ -1,4 +1,11 @@
 package local.example.outcome.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Vertex {
+
+    public String label = "";
+    public Map<Vertex, Edge> edges = new HashMap<>();
+    public boolean visited = false;
 }
