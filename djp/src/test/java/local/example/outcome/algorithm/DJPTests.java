be8a2039ac2fa51dff25b5d9f39aa90a63790f2c
paolomococci <paolomococci@gmail.com>
2022-03-07 08:54:49 +0100

    edited class DJPTests belonging to project djp

--- djp/src/test/java/local/example/outcome/algorithm/DJPTests.java
+++ djp/src/test/java/local/example/outcome/algorithm/DJPTests.java
@@ -51,4 +51,39 @@ public class DJPTests {
         graph.add(vertexE);
         Assertions.assertEquals(5, graph.vertices.size());
     }
+
+    @Test
+    public void minimumSpanningTreeTest() {
+        Graph graph = new Graph();
+        Vertex vertexA = new Vertex("A");
+        Vertex vertexB = new Vertex("B");
+        Vertex vertexC = new Vertex("C");
+        Vertex vertexD = new Vertex("D");
+        Vertex vertexE = new Vertex("E");
+        Edge edgeAB = new Edge(2);
+        Edge edgeAC = new Edge(3);
+        Edge edgeBC = new Edge(2);
+        Edge edgeBE = new Edge(5);
+        Edge edgeCD = new Edge(1);
+        Edge edgeCE = new Edge(1);
+        vertexA.add(vertexB, edgeAB);
+        vertexB.add(vertexA, edgeAB);
+        vertexA.add(vertexC, edgeAC);
+        vertexC.add(vertexA, edgeAC);
+        vertexB.add(vertexC, edgeBC);
+        vertexC.add(vertexB, edgeBC);
+        vertexB.add(vertexE, edgeBE);
+        vertexE.add(vertexB, edgeBE);
+        vertexC.add(vertexD, edgeCD);
+        vertexD.add(vertexC, edgeCD);
+        vertexC.add(vertexE, edgeCE);
+        vertexE.add(vertexC, edgeCE);
+        graph.add(vertexA);
+        graph.add(vertexB);
+        graph.add(vertexC);
+        graph.add(vertexD);
+        graph.add(vertexE);
+        // TODO
+        Assertions.assertTrue(true);
+    }
 }
