41a926e34f6d8ef86b51b5370b3e4e8ca984aeb2
paolomococci <paolomococci@gmail.com>
2022-03-07 08:18:01 +0100

    edited class DJPTests belonging to project djp

--- djp/src/test/java/local/example/outcome/algorithm/DJPTests.java
+++ djp/src/test/java/local/example/outcome/algorithm/DJPTests.java
@@ -4,6 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 
 import local.example.outcome.model.Graph;
 
+import local.example.outcome.model.Vertex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,17 @@ public class DJPTests {
     @Test
     public void createVoidGraphTest() {
         Graph graph = new Graph();
+        Assertions.assertEquals(0, graph.vertices.size());
+    }
+
+    @Test
+    public void createSampleGraphTest() {
+        Graph graph = new Graph();
+        Vertex vertexA = new Vertex("A");
+        Vertex vertexB = new Vertex("B");
+        Vertex vertexC = new Vertex("C");
+        Vertex vertexD = new Vertex("D");
+        Vertex vertexE = new Vertex("E");
         Assertions.assertEquals(0, graph.vertices.size());
     }
 }
