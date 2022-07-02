1af9f3b497242cad7675698a06f4229f8ae2fd54
paolomococci <paolomococci@gmail.com>
2022-07-02 18:17:42 +0200

    I added class OutcomeResource belonging to project eoq

---
+++ eoq/src/main/java/local/example/outcome/OutcomeResource.java
@@ -0,0 +1,16 @@
+package local.example.outcome;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/outcome")
+public class OutcomeResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String feedback() {
+        return "<!--feedback endpoint-->";
+    }
+}
