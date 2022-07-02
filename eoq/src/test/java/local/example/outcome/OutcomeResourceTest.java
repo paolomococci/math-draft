01169b1994f9f7aa4308d13176493a55ccc8a314
paolomococci <paolomococci@gmail.com>
2022-07-02 18:18:00 +0200

    I added class OutcomeResourceTest belonging to project eoq

---
+++ eoq/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -0,0 +1,21 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class OutcomeResourceTest {
+
+    @Test
+    public void testFeedbackEndpoint() {
+        given()
+          .when().get("/outcome")
+          .then()
+             .statusCode(200)
+             .body(is("<!--feedback endpoint-->"));
+    }
+}
