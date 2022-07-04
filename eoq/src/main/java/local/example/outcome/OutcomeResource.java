2b361339a89d7fc80609f5053f36702e902831d2
paolomococci <paolomococci@gmail.com>
2022-07-04 17:32:21 +0200

    I edited class OutcomeResource belonging to project eoq

--- eoq/src/main/java/local/example/outcome/OutcomeResource.java
+++ eoq/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,12 +1,22 @@
 package local.example.outcome;
 
+import local.example.outcome.model.Eoq;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 @Path("/outcome")
 public class OutcomeResource {
+
+    private final Set<Eoq> eoqs = Collections.newSetFromMap(
+            Collections.synchronizedMap(new LinkedHashMap<>())
+    );
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
