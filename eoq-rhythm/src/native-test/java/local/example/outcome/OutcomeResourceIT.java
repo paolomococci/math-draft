215b0a1854987cd6543deeb72f62492058bc0953
paolomococci <paolomococci@gmail.com>
2022-07-13 12:42:20 +0200

    I added class OutcomeResourceIT belonging to project eoq-rhythm

---
+++ eoq-rhythm/src/native-test/java/local/example/outcome/OutcomeResourceIT.java
@@ -0,0 +1,9 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class OutcomeResourceIT extends OutcomeResourceTest {
+
+    // Execute the same tests but in native mode.
+}
