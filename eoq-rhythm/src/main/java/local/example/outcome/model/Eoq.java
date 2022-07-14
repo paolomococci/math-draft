7b5f4d67f5c11816a162fc86308b1bcb24477fef
paolomococci <paolomococci@gmail.com>
2022-07-14 06:56:21 +0200

    I edited class Eoq belonging to project eoq-rhythm

--- eoq-rhythm/src/main/java/local/example/outcome/model/Eoq.java
+++ eoq-rhythm/src/main/java/local/example/outcome/model/Eoq.java
@@ -2,6 +2,14 @@ package local.example.outcome.model;
 
 public class Eoq {
 
+    public double demand;
+    public double costOfIssuing;
+    public double price;
+    public double interestRate;
+    public double costOfStock;
+    public long quantity;
+    public long ordersToProcess;
+
     public Eoq() {
     }
 }
