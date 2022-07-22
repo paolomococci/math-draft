92ce65827c447b3d73c2278d82e587cf7e7d9b51
paolomococci <paolomococci@gmail.com>
2022-07-23 01:15:47 +0200

    I edited class Eoq to project eoq-safety

--- eoq-safety/src/main/kotlin/local/example/outcome/model/Eoq.kt
+++ eoq-safety/src/main/kotlin/local/example/outcome/model/Eoq.kt
@@ -1,4 +1,12 @@
 package local.example.outcome.model
 
 class Eoq {
+
+    var demand = 0.0
+    var costOfIssuing = 0.0
+    var price = 0.0
+    var interestRate = 0.0
+    var costOfStock = 0.0
+    var quantity: Long = 0
+    var ordersToProcess: Long = 0
 }
