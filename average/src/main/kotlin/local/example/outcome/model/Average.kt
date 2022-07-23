d1ec57988d45d23c4325b2c6ead5d70ba62feb36
paolomococci <paolomococci@gmail.com>
2022-07-23 19:14:30 +0200

    I edited class Average to project average

--- average/src/main/kotlin/local/example/outcome/model/Average.kt
+++ average/src/main/kotlin/local/example/outcome/model/Average.kt
@@ -3,6 +3,7 @@ package local.example.outcome.model
 class Average() {
 
     val values: Array<Double> = emptyArray()
+    val mu: Double = 0.0
     fun setAverage() {
         TODO("Not yet implemented")
     }
