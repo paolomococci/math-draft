159453ff6fb1c0e02e40cc8ff893b93500d4fced
paolomococci <paolomococci@gmail.com>
2022-07-23 06:06:28 +0200

    I edited class OutcomeResource to project mle

--- mle/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ mle/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,5 +1,9 @@
 package local.example.outcome
 
+import local.example.outcome.model.Outcome
+
+import java.util.*
+
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -7,6 +11,12 @@ import javax.ws.rs.core.MediaType
 
 @Path("/outcome")
 class OutcomeResource {
+
+    private val outcome = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+            LinkedHashMap<Outcome, Boolean>()
+        )
+    )
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
