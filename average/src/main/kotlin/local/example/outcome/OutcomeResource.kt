852d5123865885b0bdbb6ca611065a85fdc66312
paolomococci <paolomococci@gmail.com>
2022-07-23 19:35:12 +0200

    I edited class OutcomeResource to project average

--- average/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ average/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -6,6 +6,7 @@ import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/outcome")
 class OutcomeResource {
@@ -17,6 +18,8 @@ class OutcomeResource {
     )
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun feedback() = "feedback reactive"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun read(): Response? {
+        return if (averages.isEmpty()) Response.ok().build() else Response.ok(averages).build()
+    }
 }
