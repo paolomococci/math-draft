909fba7892b4dbc6752a60a9577bd3f6a5b0e8db
paolomococci <paolomococci@gmail.com>
2022-07-24 06:50:27 +0200

    I edited class OutcomeResource to project standard-deviation

--- standard-deviation/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ standard-deviation/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -8,6 +8,7 @@ import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/outcome")
 class OutcomeResource {
@@ -19,6 +20,10 @@ class OutcomeResource {
     )
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun feedback() = "feedback reactive"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun read(): Response? {
+        return if (
+            standardDeviations.isEmpty()
+        ) Response.ok().build() else Response.ok(standardDeviations).build()
+    }
 }
