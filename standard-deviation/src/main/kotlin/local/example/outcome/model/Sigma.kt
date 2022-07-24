f242bc3bcdab5c373290cfe1f4e42d832f963df4
paolomococci <paolomococci@gmail.com>
2022-07-24 08:30:53 +0200

    I edited class Sigma to project standard-deviation

--- standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
+++ standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
@@ -1,14 +1,26 @@
 package local.example.outcome.model
 
-class Sigma(var values: Array<Double>) {
+import kotlin.math.sqrt
 
+class Sigma() {
+
+    var values: Array<Double> = emptyArray()
     var mu: Double = 0.0
     var sigma: Double = 0.0
 
-    init {
-        this.mu = this.values.average()
+    constructor(values: Array<Double>) : this() {
+        this.values = values.copyOf()
     }
     fun setStandardDeviation() {
-        TODO("Not yet implemented")
+        var sum: Double = 0.0
+        this.mu = this.values.average()
+        for (value in this.values) {
+            sum += square(value - this.mu)
+        }
+        this.sigma = sqrt(sum / this.values.size)
+    }
+
+    private fun square(difference: Double): Double {
+        return difference * difference
     }
 }
