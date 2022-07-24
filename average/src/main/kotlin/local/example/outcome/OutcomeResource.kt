04cfb70d3c85745b576d54def182b6f87e59e273
paolomococci <paolomococci@gmail.com>
2022-07-24 05:47:52 +0200

    I edited class OutcomeResource to project average

--- average/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ average/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -2,9 +2,7 @@ package local.example.outcome
 
 import local.example.outcome.model.Average
 import java.util.*
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
@@ -21,5 +19,14 @@ class OutcomeResource {
     @Produces(MediaType.APPLICATION_JSON)
     fun read(): Response? {
         return if (averages.isEmpty()) Response.ok().build() else Response.ok(averages).build()
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun compute(average: Average): Set<Average?>? {
+        average.setAverage()
+        averages.add(average)
+        return averages
     }
 }
