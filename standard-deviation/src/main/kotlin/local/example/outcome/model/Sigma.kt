10eb061f1220b16375c80e9079558c869b787b13
paolomococci <paolomococci@gmail.com>
2022-07-24 07:03:19 +0200

    I edited class Sigma to project standard-deviation

--- standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
+++ standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
@@ -1,6 +1,13 @@
 package local.example.outcome.model
 
-class Sigma {
+class Sigma(var values: Array<Double>) {
+
+    var mu: Double = 0.0
+    var sigma: Double = 0.0
+
+    init {
+        this.mu = this.values.average()
+    }
     fun setStandardDeviation() {
         TODO("Not yet implemented")
     }
