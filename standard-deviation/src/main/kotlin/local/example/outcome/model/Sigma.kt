caf1ba6ba12504aeb7a621f33c91fcae75113c06
paolomococci <paolomococci@gmail.com>
2022-07-24 08:32:25 +0200

    I edited class Sigma to project standard-deviation

--- standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
+++ standard-deviation/src/main/kotlin/local/example/outcome/model/Sigma.kt
@@ -12,11 +12,9 @@ class Sigma() {
         this.values = values.copyOf()
     }
     fun setStandardDeviation() {
-        var sum: Double = 0.0
+        var sum = 0.0
         this.mu = this.values.average()
-        for (value in this.values) {
-            sum += square(value - this.mu)
-        }
+        for (value in this.values) sum += square(value - this.mu)
         this.sigma = sqrt(sum / this.values.size)
     }
 
