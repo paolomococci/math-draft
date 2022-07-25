b22fe79a796000127dadfb9fa068163732a10ac4
paolomococci <paolomococci@gmail.com>
2022-07-25 06:20:05 +0200

    I edited class OutcomeResourceTest to project eoq-safety

--- eoq-safety/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 class OutcomeResourceTest {
 
     @Test
-    fun testFeedbackEndpoint() {
+    fun testReadEndpoint() {
         given()
           .`when`().get("/outcome")
           .then()
