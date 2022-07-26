ce3fd29e8d005f12ca85cb4b144c6bda71d1e706
paolomococci <paolomococci@gmail.com>
2022-07-26 16:58:20 +0200

    I edited class OutcomeResourceTest belonging to project eoq-stock

--- eoq-stock/src/test/java/local/example/outcome/OutcomeResourceTest.java
+++ eoq-stock/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -17,7 +17,8 @@ public class OutcomeResourceTest {
     private static final String JSON_DATA = """
             {"demand":800,"costOfIssuing":14,"price":73,"interestRate":0.12,"costOfStock":1}
             """;
-    private static final String JSON_OUTCOME = "[{\"demand\":800.0,\"costOfIssuing\":14.0,\"price\":73.0,\"interestRate\":0.12,\"costOfStock\":1.0,\"quantity\":46,\"ordersToProcess\":17}]";
+    private static final String JSON_OUTCOME =
+            "[{\"demand\":800.0,\"costOfIssuing\":14.0,\"price\":73.0,\"interestRate\":0.12,\"costOfStock\":1.0,\"quantity\":46,\"ordersToProcess\":17}]";
 
     @Test
     public void testReadEndpoint() {
