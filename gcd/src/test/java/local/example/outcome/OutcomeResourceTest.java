f0ac6e2e82d20295633d31a198895db14ecbe167
paolomococci <paolomococci@gmail.com>
2022-07-26 16:59:44 +0200

    I edited class OutcomeResourceTest belonging to project gcd

--- gcd/src/test/java/local/example/outcome/OutcomeResourceTest.java
+++ gcd/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -2,10 +2,12 @@ package local.example.outcome;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class OutcomeResourceTest {
@@ -23,5 +25,13 @@ public class OutcomeResourceTest {
           .when().get("/outcome")
           .then()
              .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+                .body(JSON_DATA).when().post(BASE_PATH)
+                .then().statusCode(HttpStatus.SC_OK)
+                .body(is(JSON_OUTCOME));
     }
 }
