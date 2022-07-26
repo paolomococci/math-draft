05b16f3b7766b1c5bca5278730f329c89bd5b041
paolomococci <paolomococci@gmail.com>
2022-07-26 14:24:13 +0200

    I edited class OutcomeResourceTest to project eoq-safety

--- eoq-safety/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -1,7 +1,11 @@
 package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
+
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+
+import org.apache.http.HttpStatus
 
 import org.hamcrest.CoreMatchers.`is`
 
@@ -15,7 +19,21 @@ class OutcomeResourceTest {
         given()
           .`when`().get("/outcome")
           .then()
-             .statusCode(200)
-             .body(`is`(""))
+             .statusCode(HttpStatus.SC_OK)
+    }
+    @Test
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"demand\":800,\"costOfIssuing\":14,\"price\":73,\"interestRate\":0.12,\"costOfStock\":1}"
+        private const val JSON_OUTCOME =
+            "[{\"demand\":800.0,\"costOfIssuing\":14.0,\"price\":73.0,\"interestRate\":0.12,\"costOfStock\":1.0,\"quantity\":46,\"ordersToProcess\":17}]"
     }
 }
