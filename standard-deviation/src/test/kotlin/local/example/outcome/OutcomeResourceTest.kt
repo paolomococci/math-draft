ef18e881b492421f8df205954cc3c5e362e7d1f8
paolomococci <paolomococci@gmail.com>
2022-07-26 16:52:21 +0200

    I edited class OutcomeResourceTest to project standard-deviation

--- standard-deviation/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ standard-deviation/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,6 +2,9 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+
+import org.apache.http.HttpStatus
 
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
@@ -15,5 +18,21 @@ class OutcomeResourceTest {
           .`when`().get("/outcome")
           .then()
              .statusCode(200)
+    }
+
+    @Test
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"values\":[2.3,7.1,3.5,6.2,9.0]}"
+        private const val JSON_OUTCOME =
+            "[{\"values\":[2.3,7.1,3.5,6.2,9.0],\"mu\":5.619999999999999,\"sigma\":2.427673783686762}]"
     }
 }
