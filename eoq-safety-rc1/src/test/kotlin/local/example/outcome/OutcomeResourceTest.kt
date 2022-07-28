38516f1984bd88d0136354c3ad3a9c4d2346c20a
paolomococci <paolomococci@gmail.com>
2022-07-28 13:54:59 +0200

    I edited class OutcomeResourceTest to project eoq-safety-rc1

--- eoq-safety-rc1/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety-rc1/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,6 +2,8 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
@@ -9,12 +11,26 @@ import org.junit.jupiter.api.Test
 class OutcomeResourceTest {
 
     @Test
-    fun testHelloEndpoint() {
+    fun testReadEndpoint() {
         given()
-          .`when`().get("/outcome")
-          .then()
-             .statusCode(200)
-             .body(`is`("Hello from RESTEasy Reactive"))
+            .`when`().get(BASE_PATH)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
     }
 
+    @Test
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"demand\":800,\"costOfIssuing\":14,\"price\":73,\"interestRate\":0.12,\"costOfStock\":1}"
+        private const val JSON_OUTCOME =
+            "[{\"id\":\"0\",\"demand\":800.0,\"costOfIssuing\":14.0,\"price\":73.0,\"interestRate\":0.12,\"costOfStock\":1.0,\"quantity\":46,\"ordersToProcess\":17}]"
+    }
 }
