29ce17ff3957aee73c5ef5b66facbf41bdd1eb14
paolomococci <paolomococci@gmail.com>
2022-07-28 13:57:45 +0200

    I edited class Eoq to project eoq-safety-rc1

--- eoq-safety-rc1/src/main/kotlin/local/example/outcome/model/Eoq.kt
+++ eoq-safety-rc1/src/main/kotlin/local/example/outcome/model/Eoq.kt
@@ -1,6 +1,19 @@
 package local.example.outcome.model
 
+import java.util.concurrent.atomic.AtomicLong
+
 class Eoq() {
+
+    private val atomicLong = AtomicLong()
+
+    lateinit var id: String
+    var demand = 0.0
+    var costOfIssuing = 0.0
+    var price = 0.0
+    var interestRate = 0.0
+    var costOfStock = 0.0
+    var quantity: Long = 0
+    var ordersToProcess: Long = 0
 
     fun setEoq() {
         TODO("Not yet implemented")
