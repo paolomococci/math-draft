dc665998073d4440d8142c2706901fda5e412d31
paolomococci <paolomococci@gmail.com>
2022-07-31 08:01:25 +0200

    I edited class GeometricMean to project geometric-mean

--- geometric-mean/src/main/kotlin/local/example/outcome/model/GeometricMean.kt
+++ geometric-mean/src/main/kotlin/local/example/outcome/model/GeometricMean.kt
@@ -1,5 +1,7 @@
 package local.example.outcome.model
 
+import kotlin.math.*
+
 class GeometricMean() {
 
     var values: Array<Double> = emptyArray()
@@ -7,5 +9,34 @@ class GeometricMean() {
 
     constructor(values: Array<Double>) : this() {
         this.values = values.copyOf()
+    }
+
+    fun setGeoMu() {
+        this.geoMu = this.roundOff(
+            this.nthRoot(
+                this.product(this.values),
+                this.values.size
+            ),
+            5
+        )
+    }
+
+    private fun product(values: Array<Double>): Double {
+        var factors = 1.0
+        for (temp in values) {
+            factors *= temp
+        }
+        return factors
+    }
+
+    private fun nthRoot(r: Double, n: Int): Double {
+        return Math.E.pow(ln(r) / n)
+    }
+
+    private fun roundOff(value: Double, placeTo: Int): Double {
+        var place = 10.0
+        for (i in 1..placeTo)
+            place *= 10.0
+        return ((value * place).roundToLong() / place)
     }
 }
