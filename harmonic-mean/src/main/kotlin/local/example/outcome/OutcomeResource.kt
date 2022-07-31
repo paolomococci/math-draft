1c171367b8d47a30af198e9fc416de1a31f3683e
paolomococci <paolomococci@gmail.com>
2022-07-31 15:16:56 +0200

    I edited class OutcomeResource to project harmonic-mean

--- harmonic-mean/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ harmonic-mean/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,5 +1,7 @@
 package local.example.outcome
 
+import local.example.outcome.model.HarmonicMean
+import java.util.*
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -7,6 +9,12 @@ import javax.ws.rs.core.MediaType
 
 @Path("/outcome")
 class OutcomeResource {
+
+    private val harmonicMeans = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+            LinkedHashMap<HarmonicMean, Boolean>()
+        )
+    )
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
