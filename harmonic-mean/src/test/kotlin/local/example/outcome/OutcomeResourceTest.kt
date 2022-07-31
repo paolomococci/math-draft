96f9ab6db81925723b048c62a7ee2a7f774b3275
paolomococci <paolomococci@gmail.com>
2022-07-31 09:36:48 +0200

    I added class OutcomeResourceTest to project geometric-mean

---
+++ harmonic-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -0,0 +1,20 @@
+package local.example.outcome
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class OutcomeResourceTest {
+
+    @Test
+    fun testHelloEndpoint() {
+        given()
+          .`when`().get("/outcome")
+          .then()
+             .statusCode(200)
+             .body(`is`("Hello from RESTEasy Reactive"))
+    }
+
+}
