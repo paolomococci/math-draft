f2085b7ac42d5f7028ff5ff476c2edcd172ddad5
paolomococci <paolomococci@gmail.com>
2022-07-31 14:42:07 +0200

    I edited class OutcomeResourceTest to project geometric-mean

--- geometric-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ geometric-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -5,6 +5,8 @@ import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
+import org.json.JSONArray
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -23,6 +25,18 @@ class OutcomeResourceTest {
             .body(JSON_DATA).`when`().post(BASE_PATH)
             .then().statusCode(HttpStatus.SC_OK)
             .body(`is`(JSON_OUTCOME))
+    }
+
+    @Test
+    fun testComputedGeoMu() {
+        val response = given().contentType(ContentType.JSON)
+            .body(
+                "{\"values\":[1.065,-1.09,1.1,1.12]}"
+            ).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK).extract().body()
+        val jsonArray = JSONArray(response.asString())
+        val jsonObject = jsonArray.getJSONObject(0)
+        assertEquals(jsonObject["geoMu"].toString(), (1.09357).toString())
     }
 
     companion object {
