bd6f90621aed75f162477874ff44939c09d38ec6
paolomococci <paolomococci@gmail.com>
2022-07-31 18:29:58 +0200

    I edited class OutcomeResourceTest to project harmonic-mean

--- harmonic-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ harmonic-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -3,9 +3,15 @@ package local.example.outcome
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import local.example.outcome.model.HarmonicMean
 import org.apache.http.HttpStatus
 import org.json.JSONArray
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import java.util.*
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -27,8 +33,47 @@ class OutcomeResourceTest {
             .then().statusCode(HttpStatus.SC_OK).extract().body()
         val jsonArray = JSONArray(response.asString())
         val jsonObject = jsonArray.getJSONObject(0)
-        //println("--> ${jsonObject.get("harmonicMu")} <--")
-        Assertions.assertEquals(jsonObject["harmonicMu"].toString(), (243.25).toString())
+        assertEquals(jsonObject["harmonicMu"].toString(), (243.25).toString())
+    }
+
+    @Test
+    @Order(3)
+    fun testReadObjectStoredSoFar() {
+        val response = given()
+            .`when`().get(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK).extract().body()
+        val jsonArray = JSONArray(response.asString())
+        if (!jsonArray.isEmpty) {
+            val jsonObject = jsonArray.getJSONObject(0)
+            val temporaryHarmonicMean = HarmonicMean(
+                this.toArray(this.toSplit(jsonObject.get("values").toString())),
+                jsonObject["harmonicMu"].toString().toDouble()
+            )
+            assertEquals(
+                temporaryHarmonicMean.values[0].toString(),
+                (40.0).toString()
+            )
+            assertEquals(
+                temporaryHarmonicMean.harmonicMu.toString(),
+                (243.25).toString()
+            )
+        }
+    }
+
+    private fun toArray(values: Array<String>): Array<Double> {
+        val doubles = Arrays.stream(values)
+            .mapToDouble {
+                item: String -> item.toDouble()
+        }.toArray()
+        return doubles.toTypedArray()
+    }
+
+    private fun toSplit(values: String): Array<String> {
+        return values.removeSurrounding("[", "]")
+            .takeIf(String::isNotEmpty)
+            ?.split(",")
+            ?.toTypedArray()
+            ?: emptyArray()
     }
 
     companion object {
