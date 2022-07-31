723c73cedc3ebb77900eff62b3e074b8a74d5122
paolomococci <paolomococci@gmail.com>
2022-07-31 16:35:07 +0200

    I edited class OutcomeResourceTest to project harmonic-mean

--- harmonic-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ harmonic-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,11 +2,10 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
 import org.apache.http.HttpStatus
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.json.JSONArray
+import org.junit.jupiter.api.*
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -20,7 +19,21 @@ class OutcomeResourceTest {
             .then().statusCode(HttpStatus.SC_OK)
     }
 
+    @Test
+    @Order(2)
+    fun testComputeEndpoint() {
+        val response = given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK).extract().body()
+        val jsonArray = JSONArray(response.asString())
+        val jsonObject = jsonArray.getJSONObject(0)
+        //println("--> ${jsonObject.get("harmonicMu")} <--")
+        Assertions.assertEquals(jsonObject["harmonicMu"].toString(), (243.25).toString())
+    }
+
     companion object {
         private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"values\":[40,700,780,850,880,970,1000,1200]}"
     }
 }
