89369769baacf0dbbaff87fe86bcd3afb9decf74
paolomococci <paolomococci@gmail.com>
2022-07-31 15:27:34 +0200

    I edited class HarmonicMean to project harmonic-mean

--- harmonic-mean/src/main/kotlin/local/example/outcome/model/HarmonicMean.kt
+++ harmonic-mean/src/main/kotlin/local/example/outcome/model/HarmonicMean.kt
@@ -1,4 +1,14 @@
 package local.example.outcome.model
 
 class HarmonicMean() {
+
+    var values: Array<Double> = emptyArray()
+    var harmonicMu: Double = 0.0
+
+    constructor(
+        values: Array<Double>
+    ) : this() {
+        this.values = values.copyOf()
+        this.harmonicMu
+    }
 }
