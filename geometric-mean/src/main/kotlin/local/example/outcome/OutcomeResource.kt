2fc47ad7448b5fcdbe63012c2ddf4fb48c226d58
paolomococci <paolomococci@gmail.com>
2022-07-31 06:37:20 +0200

    I edited class OutcomeResource to project geometric-mean

--- geometric-mean/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ geometric-mean/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -2,9 +2,7 @@ package local.example.outcome
 
 import local.example.outcome.model.GeometricMean
 import java.util.*
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
@@ -23,5 +21,14 @@ class OutcomeResource {
         return if (
             geometricMeans.isEmpty()
         ) Response.ok().build() else Response.ok(geometricMeans).build()
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun compute(geometricMean: GeometricMean): Set<GeometricMean?>? {
+        geometricMean.setGeoMu()
+        geometricMeans.add(geometricMean)
+        return geometricMeans
     }
 }
