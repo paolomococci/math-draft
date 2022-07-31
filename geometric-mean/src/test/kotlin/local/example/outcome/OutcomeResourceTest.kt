b3e72cfa62f30d0f18db4873b830bc7869807459
paolomococci <paolomococci@gmail.com>
2022-07-31 08:01:10 +0200

    I edited class OutcomeResourceTest to project geometric-mean

--- geometric-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ geometric-mean/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,6 +2,7 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
@@ -16,7 +17,19 @@ class OutcomeResourceTest {
             .then().statusCode(HttpStatus.SC_OK)
     }
 
+    @Test
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
     companion object {
         private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"values\":[1.065,1.09,1.1,1.12]}"
+        private const val JSON_OUTCOME =
+            "[{\"values\":[1.065,1.09,1.1,1.12],\"geoMu\":1.09357}]"
     }
 }
