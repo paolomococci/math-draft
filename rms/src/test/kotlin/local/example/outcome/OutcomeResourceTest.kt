9c55949bd1eafd07966528e5bbef4222166a9a81
paolomococci <paolomococci@gmail.com>
2022-08-01 18:23:19 +0200

    I edited class OutcomeResourceTest to project rms

--- rms/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ rms/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -3,7 +3,6 @@ package local.example.outcome
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.apache.http.HttpStatus
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
