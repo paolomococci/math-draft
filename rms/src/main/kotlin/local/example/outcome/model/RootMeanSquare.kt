9620e1cb2dcb9d545a8c7a60f6418efacaa9404e
paolomococci <paolomococci@gmail.com>
2022-08-01 17:45:28 +0200

    I edited class RootMeanSquare to project rms

--- rms/src/main/kotlin/local/example/outcome/model/RootMeanSquare.kt
+++ rms/src/main/kotlin/local/example/outcome/model/RootMeanSquare.kt
@@ -4,4 +4,12 @@ class RootMeanSquare() {
 
     var values: Array<Double> = emptyArray()
     var rmsMu: Double = 0.0
+
+    constructor(
+        values: Array<Double>,
+        rmsMu: Double = 0.0
+    ) : this() {
+        this.values = values.copyOf()
+        this.rmsMu = rmsMu
+    }
 }
