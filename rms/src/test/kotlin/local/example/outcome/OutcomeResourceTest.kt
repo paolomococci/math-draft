13aa7ace8615a813573adbaacb47bf0982dae8cc
paolomococci <paolomococci@gmail.com>
2022-08-01 18:23:07 +0200

    I edited class OutcomeResourceTest to project rms

--- rms/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ rms/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,18 +2,26 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class OutcomeResourceTest {
 
     @Test
-    fun testHelloEndpoint() {
+    @Order(1)
+    fun testReadEndpoint() {
         given()
-          .`when`().get("/outcome")
-          .then()
-             .statusCode(200)
-             .body(`is`("Hello from RESTEasy Reactive"))
+            .`when`().get(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
     }
 }
