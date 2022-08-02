ef6510f5134679e7ee8c86079a4262dbf0c99f2e
paolomococci <paolomococci@gmail.com>
2022-08-02 17:31:43 +0200

    I edited class OutcomeResourceTest to project eoq-safety-rc1

--- eoq-safety-rc1/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety-rc1/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -5,6 +5,7 @@ import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -19,17 +20,21 @@ class OutcomeResourceTest {
     }
 
     @Test
+    @Order(2)
     fun testComputeEndpoint() {
         given().contentType(ContentType.JSON)
             .body(JSON_DATA).`when`().post(BASE_PATH)
             .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
     }
 
     companion object {
         private const val BASE_PATH = "/outcome"
         private const val JSON_DATA =
             """
-                {"demand":8400,"standardDeviationPerDay":100.6,"procurementLeadTime":9,"serviceLevelKey":2.06,"costOfIssuing":14,"price":73,"interestRate":0.12,"costOfStock":1}
+                {"demand":43800,"standardDeviationPerDay":100.6,"procurementLeadTime":9,"serviceLevelKey":2.06,"costOfIssuing":14,"price":73,"interestRate":0.12,"costOfStock":1}
             """
+        private const val JSON_OUTCOME =
+            "[{\"id\":\"0\",\"demand\":43800.0,\"standardDeviationPerDay\":100.6,\"procurementLeadTime\":9.0,\"serviceLevelKey\":2.06,\"costOfIssuing\":14.0,\"price\":73.0,\"interestRate\":0.12,\"costOfStock\":1.0,\"quantity\":338,\"ordersToProcess\":130,\"safetyStock\":622,\"reorderLevel\":1702}]"
     }
 }
