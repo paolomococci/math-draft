2110314d5e1c5093707fdc4afc8ce2518c523bad
paolomococci <paolomococci@gmail.com>
2022-08-04 18:30:56 +0200

    I added class OutcomeResource to project eoq-safety-rc2

---
+++ eoq-safety-rc2/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -0,0 +1,14 @@
+package local.example.outcome
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/outcome")
+class OutcomeResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "Hello from RESTEasy Reactive"
+}
