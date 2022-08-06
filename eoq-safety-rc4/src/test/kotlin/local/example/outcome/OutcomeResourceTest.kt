9f47639f0347ac31f6910b2855aa85a1c9e4380b
paolomococci <paolomococci@gmail.com>
2022-08-06 20:16:15 +0200

    I edited class OutcomeResourceTest belonging to project eoq-safety-rc4

--- eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -92,6 +92,6 @@ class OutcomeResourceTest {
                 {"demand":4400.0,"sigmaDemand":11.18,"procurementLeadTime":5.0,"sigmaProcurementLeadTime":1.0,"serviceLevelKey":2.06,"costOfIssuing":20.5,"price":10.0,"interestRate":0.12,"stockRate":0.01,"spaceRate":0.06}
             """
         private const val JSON_OUTCOME =
-            "[{\"id\":\"0\",\"demand\":4400.0,\"sigmaDemand\":11.18,\"procurementLeadTime\":5.0,\"sigmaProcurementLeadTime\":1.0,\"serviceLevelKey\":2.06,\"costOfIssuing\":20.5,\"price\":10.0,\"interestRate\":0.12,\"stockRate\":0.01,\"spaceRate\":0.06,\"quantity\":308,\"ordersToProcess\":14,\"cycleStock\":154,\"safetyStock\":66,\"reorderLevel\":166,\"stockMaintenanceCost\":418.0,\"totalCostOfIssuingOrders\":null,\"totalCostOfStock\":null}]"
+            "[{\"id\":\"0\",\"demand\":4400.0,\"sigmaDemand\":11.18,\"procurementLeadTime\":5.0,\"sigmaProcurementLeadTime\":1.0,\"serviceLevelKey\":2.06,\"costOfIssuing\":20.5,\"price\":10.0,\"interestRate\":0.12,\"stockRate\":0.01,\"spaceRate\":0.06,\"quantity\":308,\"ordersToProcess\":14,\"cycleStock\":154,\"safetyStock\":66,\"reorderLevel\":166,\"stockMaintenanceCost\":418.0,\"totalCostOfIssuingOrders\":287.0,\"totalCostOfStock\":null}]"
     }
 }
