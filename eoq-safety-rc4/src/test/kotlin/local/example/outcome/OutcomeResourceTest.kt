a42bb71bba45b884f55c1b6644ed38980f6b877b
paolomococci <paolomococci@gmail.com>
2022-08-06 07:19:02 +0200

    I edited class OutcomeResourceTest belonging to project eoq-safety-rc4

--- eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -2,6 +2,7 @@ package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.MethodOrderer
@@ -20,6 +21,15 @@ class OutcomeResourceTest {
             .`when`().get(BASE_PATH)
             .then()
             .statusCode(HttpStatus.SC_OK)
+    }
+
+    @Test
+    @Order(2)
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
     }
 
     companion object {
