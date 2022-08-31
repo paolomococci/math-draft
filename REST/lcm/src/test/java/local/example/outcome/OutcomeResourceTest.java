b01886cb411a1f7303c212e9d5603bb9d44ade18
paolomococci <paolomococci@gmail.com>
2022-08-31 21:16:59 +0200

    moved:   REST/lcm/src/test/java/local/example/outcome/OutcomeResourceTest.java

---
+++ REST/lcm/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -0,0 +1,38 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import io.restassured.http.ContentType;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class OutcomeResourceTest {
+
+    private static final String BASE_PATH = "/outcome";
+    private static final String JSON_DATA = """
+            {"a":405,"b":450}
+            """;
+    private static final String JSON_OUTCOME =
+            "[{\"a\":405,\"b\":450,\"lcm\":4050}]";
+
+    @Test
+    public void readEndpointTest() {
+        given()
+          .when().get(BASE_PATH)
+          .then()
+             .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+                .body(JSON_DATA).when().post(BASE_PATH)
+                .then().statusCode(HttpStatus.SC_OK)
+                .body(is(JSON_OUTCOME));
+    }
+}
