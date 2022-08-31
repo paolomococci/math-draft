a0575250276693a97f82507efb3be6b9e34e468a
paolomococci <paolomococci@gmail.com>
2022-08-31 21:16:12 +0200

    moved:   REST/lcm/src/main/java/local/example/outcome/OutcomeResource.java

---
+++ REST/lcm/src/main/java/local/example/outcome/OutcomeResource.java
@@ -0,0 +1,38 @@
+package local.example.outcome;
+
+import local.example.outcome.model.Lcm;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@Path("/outcome")
+public class OutcomeResource {
+
+    private final Set<Lcm> lcms = Collections.newSetFromMap(
+            Collections.synchronizedMap(
+                    new LinkedHashMap<>()
+            )
+    );
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response read() {
+        if (lcms.isEmpty()) {
+            return Response.ok().build();
+        }
+        return Response.ok(this.lcms).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<Lcm> compute(Lcm lcm) {
+        lcm.setLcm();
+        lcms.add(lcm);
+        return lcms;
+    }
+}
