c635c9de34c502b8cb4bab49016ca239341f8c74
paolomococci <paolomococci@gmail.com>
2022-08-31 21:14:20 +0200

    moved:   REST/knapsack/src/main/java/local/example/outcome/model/Backpack.java

---
+++ REST/knapsack/src/main/java/local/example/outcome/model/Backpack.java
@@ -0,0 +1,15 @@
+package local.example.outcome.model;
+
+public class Backpack {
+
+
+    public String name;
+    public Rack rack;
+
+    public Backpack() {
+    }
+
+    public Backpack(Rack rack) {
+        this.rack = rack;
+    }
+}
