08cec8457487ec70116b0b7e95acb989644754a3
paolomococci <paolomococci@gmail.com>
2022-08-31 20:28:34 +0200

    moved:   REST/eoq-discount/src/test/java/local/example/outcome/OutcomeResourceTest.java

---
+++ REST/eoq-discount/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -0,0 +1,22 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class OutcomeResourceTest {
+
+    @Test
+    public void testReadEndpoint() {
+        given()
+                .when().get("/outcome")
+                .then()
+                .statusCode(200)
+                .body(is(""));
+    }
+}
