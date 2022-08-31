229a978890d4d2f33a38e9af2a08155366d43b28
paolomococci <paolomococci@gmail.com>
2022-08-31 21:14:30 +0200

    moved:   REST/knapsack/src/main/java/local/example/outcome/model/Rack.java

---
+++ REST/knapsack/src/main/java/local/example/outcome/model/Rack.java
@@ -0,0 +1,38 @@
+package local.example.outcome.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rack {
+
+
+    public String name;
+    public final List<Item> items;
+
+    public Rack() {
+        items = new ArrayList<>();
+    }
+
+    public void add(Item item) {
+        this.items.add(item);
+    }
+
+    public Item get(int index) {
+        return this.items.get(index);
+    }
+
+    public List<Item> toList() {
+        return this.items.stream().toList();
+    }
+
+    public Item[] toArray() {
+        Item[] arrayOfItem = new Item[this.items.size()];
+        arrayOfItem = this.items.toArray(arrayOfItem);
+        return arrayOfItem;
+    }
+
+    public void fromArray(Item[] arrayOfItem) {
+        this.items.clear();
+        this.items.addAll(List.of(arrayOfItem));
+    }
+}
