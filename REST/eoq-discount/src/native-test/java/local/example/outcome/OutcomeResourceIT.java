74cf9e65da30ffac905541fcdcd42e3b7826db53
paolomococci <paolomococci@gmail.com>
2022-08-31 20:28:27 +0200

    moved:   REST/eoq-discount/src/native-test/java/local/example/outcome/OutcomeResourceIT.java

---
+++ REST/eoq-discount/src/native-test/java/local/example/outcome/OutcomeResourceIT.java
@@ -0,0 +1,8 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class OutcomeResourceIT
+        extends OutcomeResourceTest {
+}
