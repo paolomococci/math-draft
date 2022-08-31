a94aed9883c482dbedbb8a06bf06472f6a8e0500
paolomococci <paolomococci@gmail.com>
2022-08-31 21:29:48 +0200

    moved:   REST/standard-deviation/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt

---
+++ REST/standard-deviation/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -0,0 +1,38 @@
+package local.example.outcome
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+
+import org.apache.http.HttpStatus
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class OutcomeResourceTest {
+
+    @Test
+    fun testReadEndpoint() {
+        given()
+          .`when`().get(BASE_PATH)
+          .then()
+             .statusCode(HttpStatus.SC_OK)
+    }
+
+    @Test
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"values\":[2.3,7.1,3.5,6.2,9.0]}"
+        private const val JSON_OUTCOME =
+            "[{\"values\":[2.3,7.1,3.5,6.2,9.0],\"mu\":5.619999999999999,\"sigma\":2.427673783686762}]"
+    }
+}
