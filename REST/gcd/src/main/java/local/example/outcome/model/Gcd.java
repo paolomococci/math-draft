55214e22727e94b5e65467a4ee1e7b89c619b392
paolomococci <paolomococci@gmail.com>
2022-08-31 20:48:35 +0200

    moved:   REST/gcd/src/main/java/local/example/outcome/model/Gcd.java

---
+++ REST/gcd/src/main/java/local/example/outcome/model/Gcd.java
@@ -0,0 +1,28 @@
+package local.example.outcome.model;
+
+public class Gcd {
+
+    public int a;
+    public int b;
+    public int gcd;
+
+    public Gcd() {
+    }
+
+    public Gcd(int a, int b) {
+        this.a = Math.abs(a);
+        this.b = Math.abs(b);
+    }
+
+    public void setGcd() {
+        this.gcd = this.greatestCommonDivisor(this.a, this.b);
+    }
+
+    private int greatestCommonDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        int temp = a % b;
+        return greatestCommonDivisor(b, temp);
+    }
+}
