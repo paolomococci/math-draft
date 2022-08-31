72028d911811c659c570db5a2f7e0e645e8c1c1d
paolomococci <paolomococci@gmail.com>
2022-08-31 21:27:42 +0200

    moved:   REST/sieve/src/test/java/local/example/outcome/OutcomeResourceTest.java

---
+++ REST/sieve/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -0,0 +1,19 @@
+package local.example.outcome;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class OutcomeResourceTest {
+
+    @Test
+    public void readEndpointTest() {
+        given()
+          .when().get("/outcome")
+          .then()
+             .statusCode(200);
+    }
+}
