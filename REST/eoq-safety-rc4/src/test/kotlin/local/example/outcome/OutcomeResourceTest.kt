75b3282ac64891a7a62caa47a203238040dfe3f7
paolomococci <paolomococci@gmail.com>
2022-08-31 20:41:47 +0200

    moved:   REST/eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt

---
+++ REST/eoq-safety-rc4/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -0,0 +1,117 @@
+package local.example.outcome
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import local.example.outcome.model.Eoq
+import org.apache.http.HttpStatus
+import org.hamcrest.CoreMatchers.`is`
+import org.json.JSONArray
+import org.junit.jupiter.api.*
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class OutcomeResourceTest {
+
+    @Test
+    @Order(1)
+    fun testReadEndpoint() {
+        given()
+            .`when`().get(BASE_PATH)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+    }
+
+    @Test
+    @Order(2)
+    fun testComputeEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
+    @Test
+    @Order(3)
+    fun testReadObjectStoredSoFar() {
+        val response = given()
+            .`when`().get(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK).extract().body()
+        val jsonArray = JSONArray(response.asString())
+        if (!jsonArray.isEmpty) {
+            val jsonObject = jsonArray.getJSONObject(0)
+            val eoq = Eoq(
+                jsonObject["id"].toString(),
+                jsonObject["demand"].toString().toDouble(),
+                jsonObject["sigmaDemand"].toString().toDouble(),
+                jsonObject["procurementLeadTime"].toString().toDouble(),
+                jsonObject["sigmaProcurementLeadTime"].toString().toDouble(),
+                jsonObject["serviceLevelKey"].toString().toDouble(),
+                jsonObject["costOfIssuing"].toString().toDouble(),
+                jsonObject["price"].toString().toDouble(),
+                jsonObject["interestRate"].toString().toDouble(),
+                jsonObject["stockRate"].toString().toDouble(),
+                jsonObject["spaceRate"].toString().toDouble(),
+                jsonObject["quantity"].toString().toLong(),
+                jsonObject["ordersToProcess"].toString().toLong(),
+                jsonObject["safetyStock"].toString().toLong(),
+                jsonObject["cycleStock"].toString().toLong(),
+                jsonObject["averageStock"].toString().toLong(),
+                jsonObject["reorderLevel"].toString().toLong(),
+                jsonObject["stockMaintenanceCost"].toString().toDouble(),
+                jsonObject["totalCostOfIssuingOrders"].toString().toDouble(),
+                jsonObject["totalCostOfStock"].toString().toDouble(),
+            )
+            Assertions.assertEquals(
+                (0).toString(),
+                eoq.id
+            )
+            Assertions.assertEquals(
+                (308).toString(),
+                eoq.quantity.toString()
+            )
+            Assertions.assertEquals(
+                (15).toString(),
+                eoq.ordersToProcess.toString()
+            )
+            Assertions.assertEquals(
+                (66).toString(),
+                eoq.safetyStock.toString()
+            )
+            Assertions.assertEquals(
+                (154).toString(),
+                eoq.cycleStock.toString()
+            )
+            Assertions.assertEquals(
+                (220).toString(),
+                eoq.averageStock.toString()
+            )
+            Assertions.assertEquals(
+                (166).toString(),
+                eoq.reorderLevel.toString()
+            )
+            Assertions.assertEquals(
+                (418.0).toString(),
+                eoq.stockMaintenanceCost.toString()
+            )
+            Assertions.assertEquals(
+                (307.5).toString(),
+                eoq.totalCostOfIssuingOrders.toString()
+            )
+            Assertions.assertEquals(
+                (725.5).toString(),
+                eoq.totalCostOfStock.toString()
+            )
+        }
+    }
+
+    companion object {
+        private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            """
+                {"demand":4400.0,"sigmaDemand":11.18,"procurementLeadTime":5.0,"sigmaProcurementLeadTime":1.0,"serviceLevelKey":2.06,"costOfIssuing":20.5,"price":10.0,"interestRate":0.12,"stockRate":0.01,"spaceRate":0.06}
+            """
+        private const val JSON_OUTCOME =
+            "[{\"id\":\"0\",\"demand\":4400.0,\"sigmaDemand\":11.18,\"procurementLeadTime\":5.0,\"sigmaProcurementLeadTime\":1.0,\"serviceLevelKey\":2.06,\"costOfIssuing\":20.5,\"price\":10.0,\"interestRate\":0.12,\"stockRate\":0.01,\"spaceRate\":0.06,\"quantity\":308,\"ordersToProcess\":15,\"safetyStock\":66,\"averageStock\":220,\"cycleStock\":154,\"reorderLevel\":166,\"stockMaintenanceCost\":418.0,\"totalCostOfIssuingOrders\":307.5,\"totalCostOfStock\":725.5}]"
+    }
+}
