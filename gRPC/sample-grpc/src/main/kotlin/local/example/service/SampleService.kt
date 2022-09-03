782979f36b879ba1feecfb680c1b1dcfcf68d289
paolomococci <paolomococci@gmail.com>
2022-09-03 21:36:33 +0200

    I edited SampleService.kt belonging to the sample-grpc project

--- gRPC/sample-grpc/src/main/kotlin/local/example/service/SampleService.kt
+++ gRPC/sample-grpc/src/main/kotlin/local/example/service/SampleService.kt
@@ -1,11 +1,13 @@
 package local.example.service
 
+import io.quarkus.grpc.GrpcService
 import io.smallrye.mutiny.Uni
 
 import local.example.hello.grpc.Greeter
 import local.example.hello.grpc.HelloReply
 import local.example.hello.grpc.HelloRequest
 
+@GrpcService
 class SampleService : Greeter {
 
     override fun sayHello(helloRequest: HelloRequest?): Uni<HelloReply> {
