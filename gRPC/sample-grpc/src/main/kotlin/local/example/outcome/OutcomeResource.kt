6d68f61cc4ba788d39585d90a996505fcb646ada
paolomococci <paolomococci@gmail.com>
2022-09-03 21:42:48 +0200

    I edited OutcomeResource.kt belonging to the sample-grpc project

--- gRPC/sample-grpc/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ gRPC/sample-grpc/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,14 +1,34 @@
 package local.example.outcome
 
+import io.quarkus.grpc.GrpcClient
+import io.smallrye.mutiny.Uni
+
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
+import local.example.hello.grpc.Greeter
+import local.example.hello.grpc.HelloReply
+import local.example.hello.grpc.HelloRequest
+
 @Path("/outcome")
 class OutcomeResource {
+
+    @GrpcClient
+    lateinit var greeter: Greeter
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = "Hello"
+
+    @GET
+    @Path("/{name}")
+    fun hello(name: String): Uni<String> {
+        return this.greeter.sayHello(
+            HelloRequest.newBuilder().setName(name).build()
+        ).onItem().transform {
+                helloReply: HelloReply -> helloReply.message
+        }
+    }
 }
