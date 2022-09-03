17cda414aa7343064e9c1c12d865ac0cf42bda1e
paolomococci <paolomococci@gmail.com>
2022-09-03 21:28:41 +0200

    I edited SampleService.kt belonging to the sample-grpc project

--- gRPC/sample-grpc/src/main/kotlin/local/example/service/SampleService.kt
+++ gRPC/sample-grpc/src/main/kotlin/local/example/service/SampleService.kt
@@ -1,4 +1,12 @@
 package local.example.service
 
-class SampleService {
+import io.smallrye.mutiny.Uni
+import local.example.hello.grpc.Greeter
+import local.example.hello.grpc.HelloReply
+import local.example.hello.grpc.HelloRequest
+
+class SampleService : Greeter {
+    override fun sayHello(request: HelloRequest?): Uni<HelloReply> {
+        TODO("Not yet implemented")
+    }
 }
