aa9d2a4e403336a46d66e0b2b254bea4ff531ea8
paolomococci <paolomococci@gmail.com>
2022-09-25 21:11:22 +0200

    edited OutcomeResourceTest.java belonging to project k-means

--- REST/k-means/src/test/java/local/example/outcome/OutcomeResourceTest.java
+++ REST/k-means/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -2,6 +2,7 @@ package local.example.outcome;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,6 +13,12 @@ import static org.hamcrest.CoreMatchers.is;
 public class OutcomeResourceTest {
 
     private static final String BASE_PATH = "/outcome";
+
+    @Test
+    public void endpointTest() {
+        given().when().get(BASE_PATH)
+                .then().statusCode(HttpStatus.SC_OK);
+    }
 
     @Test
     public void feedbackOutcomeTest() {
