516f06a62ef84bc1574026a0c8543dd18a511236
paolomococci <paolomococci@gmail.com>
2022-09-27 05:37:30 +0200

    edited ClusteringMeans.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/algorithm/ClusteringMeans.java
+++ REST/k-means/src/main/java/local/example/outcome/algorithm/ClusteringMeans.java
@@ -1,4 +1,20 @@
 package local.example.outcome.algorithm;
 
+import local.example.outcome.model.Centroid;
+import local.example.outcome.model.Distance;
+import local.example.outcome.model.Record;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ClusteringMeans {
+
+    static Map<Centroid, List<Record>> tieDown(
+            List<Record> records,
+            int numberOfClusters,
+            Distance distance,
+            int limitNumberOfIterations
+    ) {
+        return null;
+    }
 }
