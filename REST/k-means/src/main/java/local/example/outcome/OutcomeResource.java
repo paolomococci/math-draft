f30fbb7fb7ae98ca1b77115e440a20d2b2f08f37
paolomococci <paolomococci@gmail.com>
2022-09-27 21:06:36 +0200

    edited OutcomeResource.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/OutcomeResource.java
+++ REST/k-means/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,12 +1,23 @@
 package local.example.outcome;
 
+import local.example.outcome.model.ClusterView;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/outcome")
 public class OutcomeResource {
+
+    private final Set<ClusterView> clusterViews = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+                new LinkedHashMap<>()
+        )
+    );
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
