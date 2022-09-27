5aacc34faaf8c282150fd2906117650cb218a8b5
paolomococci <paolomococci@gmail.com>
2022-09-27 04:58:38 +0200

    edited EuclideanDistance.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/algorithm/EuclideanDistance.java
+++ REST/k-means/src/main/java/local/example/outcome/algorithm/EuclideanDistance.java
@@ -5,11 +5,21 @@ import local.example.outcome.model.Distance;
 import java.util.Map;
 
 public class EuclideanDistance implements Distance {
+
     @Override
     public double compute(
             Map<String, Double> featuresOne,
             Map<String, Double> featuresTwo
     ) {
+        double amount = 0.0;
+
+        for (String key : featuresOne.keySet()) {
+            Double f1 = featuresOne.get(key);
+            Double f2 = featuresTwo.get(key);
+
+            if (f1 != null && f2 != null)
+                amount += Math.pow(f1 - f2, 2);
+        }
         return 0;
     }
 }
