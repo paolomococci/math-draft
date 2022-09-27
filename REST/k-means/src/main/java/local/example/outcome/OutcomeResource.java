9f3fa031a5a9603319544c14fff3549a098e09d6
paolomococci <paolomococci@gmail.com>
2022-09-27 21:07:39 +0200

    edited OutcomeResource.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/OutcomeResource.java
+++ REST/k-means/src/main/java/local/example/outcome/OutcomeResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
@@ -20,8 +21,11 @@ public class OutcomeResource {
     );
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String feedback() {
-        return "---> outcome feedback <---";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response read() {
+        if (clusterViews.isEmpty()) {
+            return Response.ok().build();
+        }
+        return Response.ok(clusterViews).build();
     }
 }
