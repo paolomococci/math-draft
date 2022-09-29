f4b3b24749364d88ec60f582627dc721a68b1c7b
paolomococci <paolomococci@gmail.com>
2022-09-29 16:34:18 +0200

    edited SumOfSquareErrors.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/algorithm/SumOfSquareErrors.java
+++ REST/k-means/src/main/java/local/example/outcome/algorithm/SumOfSquareErrors.java
@@ -1,8 +1,18 @@
 package local.example.outcome.algorithm;
 
+import local.example.outcome.model.Centroid;
+import local.example.outcome.model.Distance;
+import local.example.outcome.model.Record;
+
+import java.util.List;
+import java.util.Map;
+
 public class SumOfSquareErrors {
 
-    public static double sumSquareErrors() {
+    public static double sumSquareErrors(
+            Map<Centroid, List<Record>> clusters,
+            Distance distance
+    ) {
         // TODO: 29/09/22
         return 0;
     }
