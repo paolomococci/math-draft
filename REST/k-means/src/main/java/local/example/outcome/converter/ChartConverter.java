963f87936a1a01cc26c8f8bcafb4618d704c0edf
paolomococci <paolomococci@gmail.com>
2022-10-01 06:37:14 +0200

    edited ChartConverter.java belonging to project k-means

--- REST/k-means/src/main/java/local/example/outcome/converter/ChartConverter.java
+++ REST/k-means/src/main/java/local/example/outcome/converter/ChartConverter.java
@@ -3,6 +3,7 @@ package local.example.outcome.converter;
 import local.example.outcome.model.Centroid;
 import local.example.outcome.model.Record;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -12,7 +13,7 @@ public class ChartConverter {
     private static Map<String, Objects> chartConverter(
             Map<Centroid, List<Record>> clusters
     ) {
-        // TODO: 01/10/22
+        Map<String, Object> json = new HashMap<>();
         return null;
     }
 }
